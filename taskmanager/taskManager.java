055fc7b3a09b8b46db75afa0b06ea2fab0372388
RevanDTH <davidkrammer21@gmail.com>
2025-07-31 22:28:44 +0200

    updated taskManager with logic

--- taskmanager/taskManager.java
+++ taskmanager/taskManager.java
@@ -1,17 +1,26 @@
 package taskmanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class taskManager {
+
+    List<task> taskList = new ArrayList<task>();
+
     public static void main(String[] args) {
         System.out.println("TaskEditor package started . . .");
     }
 
-    public void addTask(String taskName, String taskDesc){
+    public void saveTask(task taskObject){
+        taskList.add(taskObject);
     }
 
-    public void removeTask(String taskID){
+    public void removeTask(task taskObject){
+        taskList.remove(taskObject);
     }
 
-    public void getAllTasks(){
+    public List<task> getAllTasks(){
+        return taskList;
     }
 
     /*
