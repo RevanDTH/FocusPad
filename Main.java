c5f0cc5f6c9d0fd4bb19f3a7c0c674ac4aaaff0f
RevanDTH <davidkrammer21@gmail.com>
2025-08-01 13:59:01 +0200

    added main UI, added option for DebugMode in main class

--- Main.java
+++ Main.java
@@ -2,28 +2,21 @@
 import taskmanager.*;
 
 public class Main {
+
+    // Settings
+    static Boolean DebugMode = true;
+
     public static void main(String[] args) {
-        System.out.println("FocusPad started . . .");
+        if(DebugMode){System.out.println("[DEBUG] FocusPad started . . .");}
         TaskManager TaskManager = new TaskManager();
+        ui uiHandler = new ui();
+        
+        uiHandler.printMainStartUI();
 
 
 
         // tests
-        System.out.println("[DEBUG] Creating Task1");
-        Task task1 = new Task("Task1", "Testbeschreibung 1", false);
-        TaskManager.saveTask(task1);
 
-        System.out.println("[DEBUG] Creating Task2");
-        Task task2 = new Task("Task2", "Testbeschreibung 2", false);
-        TaskManager.saveTask(task2);
-
-        System.out.println("[DEBUG] Creating Task3");
-        Task task3 = new Task("Task3", "Testbeschreibung 3", false);
-        TaskManager.saveTask(task3);
-
-        TaskManager.removeTask(task3);
-
-        System.out.println("All remaining tasks: " + TaskManager.getAllTasks().toString());
         
     }
 }
--- taskmanager/ui.java
+++ taskmanager/ui.java
@@ -1,14 +1,27 @@
 package taskmanager;
 
+//imports
+import java.util.Scanner;
+
 public class ui { 
     public static void main(String[] args) {
         System.out.println("ui started . . .");
     }
 
-    public void drawStartUI(){
+
+    public void printMainStartUI(){
+        String MainUI = """
+        == FocusPad ==
+        1. Neue Aufgabe erstellen
+        2. Alle Aufgaben anzeigen
+        3. Aufgabe bearbeiten
+        4. Aufgabe löschen
+        5. Beenden        
+                """;
+        System.out.println(MainUI);
     }
 
-    public void drawTaskUI(){
+    public void printTaskUI(){
     }
 
 
